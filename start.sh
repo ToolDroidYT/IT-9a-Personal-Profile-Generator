e84546a275c38db3039ad3504955e06afdd1e38e
ToolDroid <toolboxminecraft086@gmail.com>
2026-03-06 22:43:13 +0800

    Add start.sh so I can start the php server without using the htdocs folder cuz why not.

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# You need to have php installed on your system to run this server.
+# You can check if you have it by running:
+# php -v
+
+# To run the PHP server, use the following command in the terminal:
+php -S localhost:8000 -t .
+
+# Or run the start.sh script:
+# 1. cd Activities/Laboratory/LabAct4/
+# 
+# (Only run if it says "Permission denied" or something.)
+# 2. chmod +x start.sh
+# 
+# 3. ./start.sh
+
+# Open http://localhost:8000/ in your web browser to view.
